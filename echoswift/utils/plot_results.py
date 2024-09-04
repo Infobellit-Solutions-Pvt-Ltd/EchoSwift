73226203016d2591025871e9b1efddff538cf5d5
akhilreddy0703 <akhilreddygogula2507@gmail.com>
2024-09-04 16:52:12 +0530

    updated plot_results

--- echoswift/utils/plot_results.py
+++ echoswift/utils/plot_results.py
@@ -22,10 +22,10 @@ def process_csv_files(directory_path):
 
 def write_to_csv(data, output_file):
     with open(output_file, 'w') as f:
-        f.write('Number of Parallel Users,Output Token,Token Latency (ms/tokens),Throughput (tokens/second)\n')
-        for num_users, values in sorted(data.items()):
+        f.write('Number of Parallel Requests,Output Token,Token Latency (ms/tokens),Throughput (tokens/second)\n')
+        for num_Requests, values in sorted(data.items()):
             for value in values:
-                f.write(f'{num_users},{value[0]},{value[1]},{value[2]}\n')
+                f.write(f'{num_Requests},{value[0]},{value[1]},{value[2]}\n')
 
 def plot_line_chart(data, x_label, y_label, title, output_file):
     plt.figure(figsize=(10, 6))
@@ -33,8 +33,8 @@ def plot_line_chart(data, x_label, y_label, title, output_file):
     x_values = data[x_label]
     y_values = data[y_label]
 
-    num_users = sorted(set(x_values))
-    x_ticks_positions = np.arange(len(num_users))
+    num_Requests = sorted(set(x_values))
+    x_ticks_positions = np.arange(len(num_Requests))
 
     plt.plot(x_ticks_positions, y_values, marker='o', label=y_label)
 
@@ -45,7 +45,7 @@ def plot_line_chart(data, x_label, y_label, title, output_file):
     for i, txt in enumerate(y_values):
         plt.annotate(f'{txt:.2f}', xy=(x_ticks_positions[i], y_values.iloc[i]), ha='center', va='bottom')
 
-    plt.xticks(x_ticks_positions, [str(num) for num in num_users])
+    plt.xticks(x_ticks_positions, [str(num) for num in num_Requests])
     
     plt.legend()
     plt.grid(False)
@@ -58,24 +58,24 @@ def plot_benchmark_results(base_directory):
     
     latency_throughput_data = {}
     for directory in base_directory.iterdir():
-        if directory.is_dir():
+        if directory.is_dir() and "_User" in directory.name:
             data = process_csv_files(directory)
-            for num_users, values in data.items():
-                latency_throughput_data.setdefault(num_users, []).extend(values)
+            for num_Requests, values in data.items():
+                latency_throughput_data.setdefault(num_Requests, []).extend(values)
 
     write_to_csv(latency_throughput_data, output_file)
     print(f"Aggregated data has been written to {output_file}")
 
     df = pd.read_csv(output_file)
 
-    plot_line_chart(df[['Number of Parallel Users', 'Token Latency (ms/tokens)']], 
-                    'Number of Parallel Users', 'Token Latency (ms/tokens)', 
-                    'Token Latency vs. Number of Parallel Users', 
+    plot_line_chart(df[['Number of Parallel Requests', 'Token Latency (ms/tokens)']], 
+                    'Number of Parallel Requests', 'Token Latency (ms/tokens)', 
+                    'Token Latency vs. Number of Parallel Requests', 
                     base_directory / 'token_latency_plot.png')
 
-    plot_line_chart(df[['Number of Parallel Users', 'Throughput (tokens/second)']], 
-                    'Number of Parallel Users', 'Throughput (tokens/second)', 
-                    'Throughput vs. Number of Parallel Users', 
+    plot_line_chart(df[['Number of Parallel Requests', 'Throughput (tokens/second)']], 
+                    'Number of Parallel Requests', 'Throughput (tokens/second)', 
+                    'Throughput vs. Number of Parallel Requests', 
                     base_directory / 'throughput_plot.png')
 
 if __name__ == "__main__":
